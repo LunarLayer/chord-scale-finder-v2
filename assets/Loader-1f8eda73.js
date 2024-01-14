83a85eb340a0ef378fa39f4207f3ee479eb6fc02
TomSteinmeier <113900695+LunarLayer@users.noreply.github.com>
2024-01-14 20:26:09 +0100

    Updates

--- assets/Loader-1f8eda73.js
+++
@@ -1,1 +0,0 @@
-import{p as r}from"./index-70239bf6.js";function t(){return r.jsx("div",{children:"Loading..."})}export{t as default};
--- assets/Login-be901b6b.js
+++
@@ -1,1 +0,0 @@
-import{p as i}from"./index-70239bf6.js";function o(){return i.jsx("div",{children:"Login page"})}export{o as default};
