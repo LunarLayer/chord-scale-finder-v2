1ccd1f0cf5f05f3afb554b2745db113d635116db
TomSteinmeier <113900695+LunarLayer@users.noreply.github.com>
2024-01-23 15:12:47 +0100

    Update keyChangeMenu

--- src/components/KeyChangeMenu/KeyChangeMenu.jsx
+++ src/components/KeyChangeMenu/KeyChangeMenu.jsx
@@ -4,6 +4,7 @@ import { Key, Note } from "tonal";
 import {
   highlightNotesInKey,
   selectNotesInKey,
+  setAccidental,
   setKey,
 } from "../../Features/MusicTheory/MusicTheorySlice";
 import { useEffect } from "react";
@@ -16,6 +17,7 @@ import {
 function KeyChangeMenu({ showing }) {
   const dispatch = useDispatch();
   const key = useSelector((store) => store.musicTheory.key);
+  const accidental = useSelector((store) => store.musicTheory.accidental);
   let keyNote = Note.get(key.tonic).letter;
 
   useEffect(() => {
@@ -33,6 +35,7 @@ function KeyChangeMenu({ showing }) {
     } else {
       dispatch(setKey(Key.minorKey(note + accidental)));
     }
+    dispatch(setAccidental(accidental));
     dispatch(highlightNotesInKey());
   }
 
@@ -46,34 +49,90 @@ function KeyChangeMenu({ showing }) {
   }
 
   return (
-    <div id="KeyChangeMenu">
+    <div id="KeyChangeMenu" className={showing ? "showing" : ""}>
       <div className="content">
         <div className="note-selection">
-          <div className="notes">
-            <button onClick={() => handleSetKey("C", "")}>C</button>
-            <button onClick={() => handleSetKey("D", "")}>D</button>
-            <button onClick={() => handleSetKey("E", "")}>E</button>
-            <button onClick={() => handleSetKey("F", "")}>F</button>
-            <button onClick={() => handleSetKey("G", "")}>G</button>
-            <button onClick={() => handleSetKey("A", "")}>A</button>
-            <button onClick={() => handleSetKey("B", "")}>B</button>
+          <div className="naturals">
+            <button
+              className={keyNote === "C" ? "active" : ""}
+              onClick={() => handleSetKey("C", accidental)}
+            >
+              C
+            </button>
+            <button
+              className={keyNote === "D" ? "active" : ""}
+              onClick={() => handleSetKey("D", accidental)}
+            >
+              D
+            </button>
+            <button
+              className={keyNote === "E" ? "active" : ""}
+              onClick={() => handleSetKey("E", accidental)}
+            >
+              E
+            </button>
+            <button
+              className={keyNote === "F" ? "active" : ""}
+              onClick={() => handleSetKey("F", accidental)}
+            >
+              F
+            </button>
+            <button
+              className={keyNote === "G" ? "active" : ""}
+              onClick={() => handleSetKey("G", accidental)}
+            >
+              G
+            </button>
+            <button
+              className={keyNote === "A" ? "active" : ""}
+              onClick={() => handleSetKey("A", accidental)}
+            >
+              A
+            </button>
+            <button
+              className={keyNote === "B" ? "active" : ""}
+              onClick={() => handleSetKey("B", accidental)}
+            >
+              B
+            </button>
           </div>
           <div className="accidentals">
-            <button onClick={() => handleSetKey(keyNote, "b")}>
-              {keyNote + "b"}
+            <button
+              className={accidental === "b" ? "active" : ""}
+              onClick={() => handleSetKey(keyNote, "b")}
+            >
+              {keyNote + "♭"}
             </button>
-            <button onClick={() => handleSetKey(keyNote, "")}>{keyNote}</button>
-            <button onClick={() => handleSetKey(keyNote, "#")}>
-              {keyNote + "#"}
+            <button
+              className={accidental === "" ? "active" : ""}
+              onClick={() => handleSetKey(keyNote, "")}
+            >
+              {keyNote}
+            </button>
+            <button
+              className={accidental === "#" ? "active" : ""}
+              onClick={() => handleSetKey(keyNote, "#")}
+            >
+              {keyNote + "♯"}
             </button>
           </div>
-          <div className="scaleSelection">
-            <button onClick={() => handleSelectScale("major")}>Major</button>
-            <button onClick={() => handleSelectScale("minor")}>Minor</button>
+          <div className="majorMinor">
+            <button
+              className={key.type === "major" ? "active" : ""}
+              onClick={() => handleSelectScale("major")}
+            >
+              Major
+            </button>
+            <button
+              className={key.type === "minor" ? "active" : ""}
+              onClick={() => handleSelectScale("minor")}
+            >
+              Minor
+            </button>
           </div>
         </div>
-        {/* <SharpsFlatsDisplay selected={key.tonic + key.type} /> */}
       </div>
+      {/* <SharpsFlatsDisplay selected={key.tonic + key.type} /> */}
     </div>
   );
 }
