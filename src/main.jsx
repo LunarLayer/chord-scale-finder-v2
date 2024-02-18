e8df10bbffb5d4e3e0837b8b158f62cece6e22f2
Tom Steinmeier <lunarlayer@gmail.com>
2024-02-18 15:47:29 +0100

    Rename index.css to constants.css

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import store from "./Store";
 
 import App from "./App.jsx";
-import "./index.css";
+import "./constants.css";
 import "./reset.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
